1092f931e0a3b3eae2be826ca2275177f120a4b8
Erazerlista <128334161+Erazerlista@users.noreply.github.com>
2023-05-24 21:53:37 -0700

    made corrections to path leaderboard

--- Develop/highscores.js
+++ Develop/highscores.js
@@ -1,6 +1,13 @@
 const highScoresList = document.querySelector('#highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+const backButton = document.querySelector('#backButton');
+
 
 highScoresList.innerHTML = highScores.map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
 }).join('');
+
+backButton.addEventListener('click', () => {
+    console.log('button clicked');
+    window.location.assign('/index.html');
+});
